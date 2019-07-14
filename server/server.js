d54ca165bda3d430685054863818191f6c7ece76
Jamena McInteer <jamena@webdevabq.com>
2019-07-13 23:41:20 -0500

    fixed port on app.listen

--- server/server.js
+++ server/server.js
@@ -100,4 +100,4 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public/index.html"));
 });
 
-app.listen(3001, function() {});
+app.listen(process.env.PORT || 3001, function() {});
