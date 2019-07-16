f7b2abdef5db04745c95ab5ec09ad184515f3bf6
Jamena McInteer <jamena@webdevabq.com>
2019-07-16 11:43:00 -0500

    Moved robot font into styled components

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
   body {
+    @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
     font-family: "Roboto", sans-serif;
     color: ${props => props.theme.darkFont};
   }
