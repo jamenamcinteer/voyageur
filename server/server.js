34a3814a8f0061c288e7338732874c10e4ab3aec
Jamena McInteer <jamena@webdevabq.com>
2019-07-17 09:02:07 -0500

    Fix for POST / PUT issue #16 caused by CORS (#17)
    added environment variable for cors whitelist url

--- server/server.js
+++ server/server.js
@@ -20,7 +20,11 @@ var app = express();
 
 app.use(bodyParser.json());
 
-var whitelist = ["http://localhost:3000/", "http://localhost:3001/"];
+var whitelist = [
+  "http://localhost:3000/",
+  "http://localhost:3001/",
+  config.get("CORS_URL")
+];
 var corsOptions = {
   origin: function(origin, callback) {
     // console.log(origin);
