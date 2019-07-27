62254ad2b683a92ea0b8f232f38c116d8adbc85b
Jamena McInteer <jamena@webdevabq.com>
2019-07-26 19:57:44 -0500

    added test for delete modal on trip page

--- src/__tests__/components/Trip.test.js
+++ src/__tests__/components/Trip.test.js
@@ -10,73 +10,54 @@ import {
   auth
 } from "../../testUtils/fixtures/mockStoreData";
 import { renderWithReduxRouterAndTheme } from "../../testUtils/helpers/renderHelpers";
+import { toBeInTheDocument } from "@testing-library/jest-dom";
 
-// function renderWithRedux(
-//   ui,
-//   {
-//     initialState,
-//     store = createStore(tripsReducer, tripsReducerDefaultState)
-//   } = {}
-// ) {
-//   return {
-//     ...render(<Provider store={store}>{ui}</Provider>),
-//     store
-//   };
-// }
-
-// function renderWithRouter(
-//   ui,
-//   {
-//     route = "/",
-//     history = createMemoryHistory({ initialEntries: [route] })
-//   } = {}
-// ) {
-//   return {
-//     ...render(<Router history={history}>{ui}</Router>),
-//     history
-//   };
-// }
+expect.extend({ toBeInTheDocument });
 
 afterEach(cleanup);
 
-// it("renders", () => {
-//   const { asFragment } = render(
-//     <Provider store={store}>
-//       <Trip trips={trips} />
-//     </Provider>
-//   );
-//   expect(asFragment()).toMatchSnapshot();
-// });
+// this is a silly store that can never be changed
+const store = createStore(() => ({
+  trips,
+  budgetCategories,
+  budgetItems,
+  expenses,
+  auth
+}));
 
-// test("can render with redux with defaults", () => {
-//   // const { getByTestId, getByText } = renderWithRedux(<Trip />)
-//   // fireEvent.click(getByText('+'))
-//   // expect(getByTestId('count-value').textContent).toBe('1')
-//   const { asFragment } = renderWithRedux(<Trip />);
-//   expect(asFragment()).toMatchSnapshot();
-// });
+const historyMock = { push: jest.fn() };
 
 test("can render with redux with custom store", () => {
-  // this is a silly store that can never be changed
-  const store = createStore(() => ({
-    trips,
-    budgetCategories,
-    budgetItems,
-    expenses,
-    auth
-  }));
   const { asFragment } = renderWithReduxRouterAndTheme(
     <Trip match={{ params: { id: "1" } }} />,
     {
       store
     }
   );
-  // const { getByTestId, getByText } = renderWithRedux(<Trip />, {
-  //   store,
-  // })
-  // fireEvent.click(getByText('+'))
-  // expect(getByTestId('count-value').textContent).toBe('1000')
-  // fireEvent.click(getByText('-'))
-  // expect(getByTestId('count-value').textContent).toBe('1000')
   expect(asFragment()).toMatchSnapshot();
+});
+
+test("should display modal after clicking Delete and redirect to Dashboard page after confirming delete", done => {
+  const { getByText, getByTestId, queryByText } = renderWithReduxRouterAndTheme(
+    <Trip
+      match={{ params: { id: "1" } }}
+      history={historyMock}
+      isTest={true}
+    />,
+    { store }
+  );
+
+  getByText("Delete Trip").click();
+  expect(getByText("Yes, Delete")).toBeInTheDocument();
+
+  getByTestId("closeModal").click();
+  expect(queryByText("Yes, Delete")).toBeNull();
+
+  getByText("Delete Trip").click();
+  getByText("Yes, Delete").click();
+
+  setTimeout(() => {
+    expect(historyMock.push.mock.calls[0]).toEqual([`/`]);
+    done();
+  }, 2000);
 });
