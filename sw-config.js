06dd1c926d289e2b4bb69d51e271c5b407e27fce
Jamena McInteer <jamena@webdevabq.com>
2019-07-27 22:30:51 -0500

    Updates for PWA (#27)
    * updated manifest with new graphics
    
    * updated manifest background color
    
    * updated 192px icon for pwa
    
    * test unregister service worker
    
    * add service worker config
    
    * change package for handling sw config
    
    * updated sw-config, cleanup

---
+++ sw-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  staticFileGlobs: [
+    "build/**/*.html",
+    "build/manifest.json",
+    "build/static/**/!(*map*)"
+  ],
+  staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+  swFilePath: "./build/service-worker.js",
+  stripPrefix: "build/",
+  navigateFallback: "/index.html",
+  navigateFallbackWhitelist: [/^\/trip\//, /^\/login\//],
+  runtimeCaching: [
+    {
+      urlPattern: "'/'",
+      handler: "networkFirst"
+    },
+    {
+      urlPattern: "/auth/",
+      handler: "networkOnly"
+    },
+    {
+      urlPattern: "/api/",
+      handler: "networkFirst"
+    },
+    {
+      urlPattern: "/ping/",
+      handler: "networkOnly"
+    },
+    {
+      urlPattern: "/photos/",
+      handler: "networkFirst"
+    }
+  ]
+};
